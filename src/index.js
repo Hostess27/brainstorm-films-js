00cbae390ee05993efd2751ebccd311fbe194807
VitaliiMoskalyk <vitalikmoskalik@gmail.com>
2021-11-17 22:49:00 +0200

    начало :+1: )

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './sass/main.scss';
+import searchForQuery from'./search.js';
+
+const KEY = "c69608b9bc251fbb333be1b2d7a49ce6";
+
+    searchForQuery(KEY, "Titanic", "1").then((o)=>console.log(o));
