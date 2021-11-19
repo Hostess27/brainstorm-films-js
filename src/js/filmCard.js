cebad6f8557823baf890a35f1781e0ce2c0cbaca
Vasyl Yakymets <Vasia911119@gmail.com>
2021-11-19 02:01:15 +0200

    Update styles

--- src/js/filmCard.js
+++ src/js/filmCard.js
@@ -4,7 +4,7 @@ import filmCard from '../templates/filmCard.hbs';
 const KEY = "c69608b9bc251fbb333be1b2d7a49ce6";
 
 const refs = {
-    galleryList: document.querySelector('.main-container')
+    galleryList: document.querySelector('.gallery')
 };
 
 searchForQuery(KEY, "Titanic", "1").then(({ results }) => {
