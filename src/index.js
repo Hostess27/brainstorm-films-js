49280d03b0a3474da0fd9f7c513d227e3263d197
AleksandrNebesnyi <82997944+AleksandrNebesnyi@users.noreply.github.com>
2021-11-19 14:52:13 +0200

    get-population-film

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import './sass/main.scss';
 import searchForQuery from'./search.js';
+import {fetchTrending} from './js/api';
+import { renderGalleryTrendingMovie } from './js/render';
+import { error } from './js/notify';
+
 
 const KEY = "c69608b9bc251fbb333be1b2d7a49ce6";
 
-    searchForQuery(KEY, "Titanic", "1").then((o)=>console.log(o));
+// searchForQuery(KEY, "Titanic", "1").then((o)=>console.log(o));
+
+
+window.addEventListener('DOMContentLoaded', renderMovieOnStartPage);
+function renderMovieOnStartPage() { 
+      
+fetchTrending().then((data) =>  renderGalleryTrendingMovie(data)).catch(()=> error({
+          title: 'Error!',
+          text: 'Loading Error',
+        }))};
