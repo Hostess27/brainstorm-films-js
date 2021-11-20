bfab5df870d724d15f59271b5504bf07c14f1b42
Vasyl Yakymets <Vasia911119@gmail.com>
2021-11-20 21:44:21 +0200

    delete Titanik example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './sass/main.scss';
 import './templates/filmCard.hbs';
 import './js/searchingForQuery.js';
-import './js/filmCard.js';
 import './js/spinner.js';
 import renderMovieOnStartPage from './js/renderMoviesOnStartPage';
 
