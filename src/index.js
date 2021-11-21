254f5385c29c295bcf2c706c56e1936a3de2b2e8
VitaliiMoskalyk <vitalikmoskalik@gmail.com>
2021-11-21 15:07:20 +0200

    test_version

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import './templates/filmCard.hbs';
 import './js/searchingForQuery.js';
 import './js/spinner.js';
 import renderMovieOnStartPage from './js/renderMoviesOnStartPage';
+import './js/firebaze.js';
 
 window.addEventListener('DOMContentLoaded', renderMovieOnStartPage);
