28a3df780a6c9c752d5a35fe1ea0f3db411646e1
Hostess27 <pash.pash22173@gmail.com>
2021-11-23 18:13:56 +0200

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,83 +1,80 @@
-// import { filmLoader } from "./modal-service";
-// import ModalFilmRenderer from "./modal-renderer";
-// import filmFirebaseStorage from "./film-firebase-storage";
+import { filmLoader } from "./modal-service";
+import ModalFilmRenderer from "./modal-renderer";
+import filmFirebaseStorage from "./film-firebase-storage";
 
-// (() => 
-// {
-//     const refs = 
-//     {
-//       closeModalBtn: document.querySelector('[data-modal-close]'),
-//       modal: document.querySelector('[data-modal]'),
-//       gallery: document.querySelector('.js-main-container'),
-//       backdrop: document.querySelector('.backdrop'),
-//     };
+(() => 
+{
+    const refs = 
+    {
+      closeModalBtn: document.querySelector('[data-modal-close]'),
+      modal: document.querySelector('[data-modal]'),
+      gallery: document.querySelector('.js-main-container'),
+      backdrop: document.querySelector('.backdrop'),
+    };
 
-//     let addToWatched;
-//     let addToQueue;
-//     let filmId;
+    let addToWatched;
+    let addToQueue;
+    let filmId;
 
-//     // updateBtnWatched();
+    // updateBtnWatched();
   
-//     refs.closeModalBtn.addEventListener('click', toggleModal);
-//     refs.gallery.addEventListener('click', onFilmSelected);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+    refs.gallery.addEventListener('click', onFilmSelected);
   
-//     function toggleModal() 
-//     {
-//       document.body.classList.toggle("modal-open");
-//       refs.modal.classList.toggle('backdrop--is-hidden');
-//     }
+    function toggleModal() 
+    {
+      document.body.classList.toggle("modal-open");
+      refs.modal.classList.toggle('backdrop--is-hidden');
+    }
 
-//     async function renderFilmDetails(id)
-//     {
-//       const data = await filmLoader.loadFilmById(id);
-//       ModalFilmRenderer.clear();
-//       ModalFilmRenderer.render(data);
-//       toggleModal();
-//       //buttons
-//       addToWatched = document.querySelector('#add-to-watched');
-//       addToQueue = document.querySelector('#add-to-queue');
-//       filmId = document.querySelector('.film-detail_id').dataset.id;
+    async function renderFilmDetails(id)
+    {
+      const data = await filmLoader.loadFilmById(id);
+      ModalFilmRenderer.clear();
+      ModalFilmRenderer.render(data);
+      toggleModal();
+      //buttons
+      addToWatched = document.querySelector('#add-to-watched');
+      addToQueue = document.querySelector('#add-to-queue');
+      filmId = document.querySelector('.film-detail_id').dataset.id;
     
 
-//       addToWatched.addEventListener('click', async () =>
-//       {
-//         await filmFirebaseStorage.addToWatched(filmId, addToWatched);
-//       });
+      addToWatched.addEventListener('click', async () =>
+      {
+        await filmFirebaseStorage.addToWatched(filmId, addToWatched);
+      });
 
-//       addToQueue.addEventListener('click', async () =>
-//       {
-//         await filmFirebaseStorage.addToQueue(filmId, addToQueue);
-//       });
+      addToQueue.addEventListener('click', async () =>
+      {
+        await filmFirebaseStorage.addToQueue(filmId, addToQueue);
+      });
 
-//       await filmFirebaseStorage.findFilmWatchedById(filmId, addToWatched);
-//       await filmFirebaseStorage.findFilmQueueById(filmId, addToQueue);
-//     }
-
-
-//     async function onFilmSelected(evt)
-//     {
-//       const element = evt.target.closest('.gallery__item');
-//       if(element && element.dataset.id != undefined)
-//       {
-//         console.log("ID = ", element.dataset.id);
-//         console.log(await filmLoader.loadFilmById(element.dataset.id));
-//         renderFilmDetails(element.dataset.id);
-//       }
-//     }
-
-//     refs.backdrop.addEventListener('click', evt =>
-//     {
-//       const element = evt.target.closest('.modal');
-//       if(element) return;
-//        toggleModal();
-//     });
-
-//     //Слушатель Закрытие подалки по Esc
-//     window.addEventListener('keydown', evt =>
-//     {
-//         if(evt.code === "Escape" && document.body.classList.contains('modal-open')) toggleModal();
-//     });
-//   })();
+      await filmFirebaseStorage.findFilmWatchedById(filmId, addToWatched);
+      await filmFirebaseStorage.findFilmQueueById(filmId, addToQueue);
+    }
 
 
-console.log()
+    async function onFilmSelected(evt)
+    {
+      const element = evt.target.closest('.gallery__item');
+      if(element && element.dataset.id != undefined)
+      {
+        console.log("ID = ", element.dataset.id);
+        console.log(await filmLoader.loadFilmById(element.dataset.id));
+        renderFilmDetails(element.dataset.id);
+      }
+    }
+
+    refs.backdrop.addEventListener('click', evt =>
+    {
+      const element = evt.target.closest('.modal');
+      if(element) return;
+       toggleModal();
+    });
+
+    //Слушатель Закрытие подалки по Esc
+    window.addEventListener('keydown', evt =>
+    {
+        if(evt.code === "Escape" && document.body.classList.contains('modal-open')) toggleModal();
+    });
+  })();
