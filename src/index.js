8b6517f913e375c5a4dbceb80a592d58d759579a
MariiaYaremko <83749699+MariiaYaremko@users.noreply.github.com>
2021-11-24 18:00:06 +0200

    find-films_up
    "Поиск фильмов" їде вгору при натисканні на input

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './js/searchingForQuery.js';
 import './js/spinner.js';
 import renderMovieOnStartPage from './js/renderMoviesOnStartPage';
 import './js/firebaze.js';
-import './js/heard'
+import './js/heard';
+import './js/search-form-up.js';
 
 window.addEventListener('DOMContentLoaded', renderMovieOnStartPage);
