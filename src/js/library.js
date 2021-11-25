c95cc69e3fa9ba260d0c34c745a63ac504fe2900
AnatoliyBeychuk <abis01@ukr.net>
2021-11-25 17:56:40 +0200

    Забыл про Queue :)

--- src/js/library.js
+++ src/js/library.js
@@ -77,6 +77,10 @@ function loadQueue()
             filmsIdArr.map(async id => 
             {
                 const film = await filmLoader.loadFilmById(Number(id));
+                if(film.genres.length >= 3)
+                {
+                    film.genres = [...film.genres.slice(0, 3), {id: "00000", name: "other..."}];
+                }
                 libraryRenderer.render(film);
             });
         } 
