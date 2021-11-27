310465864657eeb00fc1b9cb095f576ee0eef9a1
Vasyl Yakymets <Vasia911119@gmail.com>
2021-11-27 15:28:04 +0200

    Fix connecting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './sass/main.scss';
 import './templates/filmCard.hbs';
 import './js/searchingForQuery.js';
+import './js/themeSwitcher'
 import './js/spinner.js';
 import './js/render-movies-on-start-page';
 import './js/firebaze.js';
@@ -11,3 +12,4 @@ import './js/pagination.js';
 import './js/scroll.js';
 import './js/library.js';
 import './js/modal.js';
+import './js/modalTeam.js'
