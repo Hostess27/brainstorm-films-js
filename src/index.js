566779ffb78292c73d75ecfc9986e059db907677
MariiaYaremko <83749699+MariiaYaremko@users.noreply.github.com>
2021-11-28 11:01:27 +0200

    Update input
    Додана довжина інпута. Почищений код.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,5 @@ import './js/spinner.js';
 import renderMovieOnStartPage from './js/renderMoviesOnStartPage';
 import './js/firebaze.js';
 import './js/heard';
-import './js/search-form-up.js';
 
 window.addEventListener('DOMContentLoaded', renderMovieOnStartPage);
